7a2bb3baad1bd04a1349b62542972ce704cfd248
javierrcyb <jsr38819@email.vcss.edu>
2025-08-17 23:33:30 -0400

    Iniciando estilos

--- webpage/traductor-lsp/src/components/MainFooter.jsx
+++ webpage/traductor-lsp/src/components/MainFooter.jsx
@@ -1,7 +1,39 @@
+import "./MainFooter.css";
 
 function MainFooter() {
-    return(
-        <footer></footer>
+    return (
+        <footer>
+            <div className="container footer-content">
+                <p>© {new Date().getFullYear()} Javier Ramirez – MIT License</p>
+                <nav>
+                    <ul>
+                        <li>
+                            <a
+                                href="https://www.linkedin.com/in/javier-ramirez-737596302/"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                LinkedIn
+                            </a>
+                        </li>
+                        <li>
+                            <a
+                                href="https://github.com/javierrcyb"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                GitHub
+                            </a>
+                        </li>
+                        <li>
+                            <a href="mailto:javier.ramirez.tech@gmail.com">
+                                Contacto
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </footer>
     );
 }
 
--- webpage/traductor-lsp/src/components/MainNavBar.jsx
+++ webpage/traductor-lsp/src/components/MainNavBar.jsx
@@ -1,10 +1,45 @@
+import "./MainNavBar.css"
+import { useState } from "react";
 
-function MainNavBar(){
-    return (
-        <nav>
-            <p>Aca va el nav bar para despues</p>
-        </nav>
-    )
+function MainNavBar() {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <nav>
+        <div className="container navbar-content">
+      {/* Logo lado izquierdo */}
+      <div className="navbar-left">
+        <span className="logo">🌐</span> 
+      </div>
+
+      {/* Secciones del medio */}
+      <ul className="navbar-center">
+        <li><a href="#">Sección 1</a></li>
+        <li><a href="#">Sección 2</a></li>
+
+        {/* Sección con desplegable */}
+        <li className="dropdown">
+          <button onClick={() => setOpen(!open)}>
+            Sección 3 ▾
+          </button>
+          {open && (
+            <ul className="dropdown-menu">
+              <li><a href="#">Subsección A</a></li>
+              <li><a href="#">Subsección B</a></li>
+              <li><a href="#">Subsección C</a></li>
+            </ul>
+          )}
+        </li>
+      </ul>
+
+      {/* Botones lado derecho */}
+      <div className="navbar-right">
+        <button className="btn-signin">Sign In</button>
+        <button className="btn-signup">Sign Up</button>
+      </div>
+      </div>
+    </nav>
+  );
 }
 
-export default MainNavBar
+export default MainNavBar;
